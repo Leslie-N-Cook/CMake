9779b66ba5c798fe68cdd7566160fae4de195d0f
Leslie-N-Cook <leslienichole.co@gmail.com>
2023-05-23 17:59:54 -0500

    cmake build library

---
+++ lecture-03/make_lib/src/appStudent/build/CMakeFiles/Name.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Name.dir/Student.cpp.o"
+  "CMakeFiles/Name.dir/Student.cpp.o.d"
+  "libName.a"
+  "libName.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lecture-03/make_lib/src/appStudent/build/CMakeFiles/student_service.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lesliecook/CMake_tutorials/lecture-03/make_lib/src/appStudent/Main.cpp" "CMakeFiles/student_service.dir/Main.cpp.o" "gcc" "CMakeFiles/student_service.dir/Main.cpp.o.d"
+  "/Users/lesliecook/CMake_tutorials/lecture-03/make_lib/src/appStudent/Student.cpp" "CMakeFiles/student_service.dir/Student.cpp.o" "gcc" "CMakeFiles/student_service.dir/Student.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
